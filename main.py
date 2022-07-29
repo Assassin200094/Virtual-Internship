99b67b614c6b0bba08c33108280abf13d8a291d7
Assassin20094 <thepunisher20199@gmail.com>
2022-07-29 17:59:21 +0300

    Спринт 3

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from collections import defaultdict
-from schemas import Response, AddPass, get_pass, Pass, PassOptional, pass_details, ResponseUpdate, get_user_passes
+from schemas import Response, AddPass, get_pass, PassOptional, pass_details, ResponseUpdate, get_user_passes
 
 
 app = FastAPI()
+
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
@@ -23,11 +24,11 @@ async def validation_exception_handler(request, exc):
         ),
     )
 
+
 @app.post("/addpass", response_model = Response)
 def submitData(passes: AddPass):
     try:
         return Response(status=200, message="Отправлено успешно", id=passes.add_pass())
-
     except:
         return Response(status=500, message="Ошибка подключения к базе данных")
 
@@ -39,17 +40,24 @@ def get_pass_by_id(pass_id: int):
 
 @app.patch("/update/{pass_id}", response_model=ResponseUpdate)
 def update_pass_by_id(pass_id: int, passes: PassOptional):
-    stored_data = pass_details(pass_id)
-    if stored_data.get("status") == "new":
-        stored_model = PassOptional(**stored_data)
-        update_data = passes.dict(exclude_unset=True)
-        updated_pass = stored_model.copy(update=update_data)
-        updated_pass.update_pass(pass_id)
-        return ResponseUpdate(state=1, message="Успешное обновление")
-    else:
-        return ResponseUpdate(state=0, message="Нельзя редактировать эту запись")
+    try:
+        stored_data = pass_details(pass_id)
+        if stored_data:
+            if stored_data.get("status") == "new":
+                stored_model = PassOptional(**stored_data)
+                update_data = passes.dict(exclude_unset=True)
+                updated_pass = stored_model.copy(update=update_data)
+                updated_pass.update_pass(pass_id)
+                return ResponseUpdate(state=1, message="Успешное обновление")
+            else:
+                return ResponseUpdate(state=0, message="Нельзя редактировать эту запись")
+        else:
+            return ResponseUpdate(state=0, message="Перевал не найден")
+    except:
+        return ResponseUpdate(state=0, message="Ошибка подключения к базе данных")
 
 
 @app.get("/passes/users/{user_email}")
 def get_passes_by_email(user_email: str):
     return get_user_passes(user_email)
+
--- schemas.py
+++ schemas.py
@@ -1,24 +1,15 @@
-import os
-from dotenv import load_dotenv
 from pydantic import BaseModel
 from datetime import datetime
 from typing import List, Union, Optional
-import psycopg2
-
-
-load_dotenv()
-
-FSTR_DB_LOGIN = os.getenv('FSTR_DB_LOGIN')
-FSTR_DB_PASS = os.getenv('FSTR_DB_PASS')
-FSTR_DB_HOST = os.getenv('FSTR_DB_HOST')
-FSTR_DB_PORT = os.getenv('FSTR_DB_PORT')
-FSTR_DB_NAME = os.getenv('FSTR_DB_NAME')
+from fastapi import HTTPException
+from db_connection import connect_db
 
 
 class User(BaseModel):
     name: str
     email: str
     phone: str = None
+
 
 class Pass(BaseModel):
     beauty_title: str
@@ -47,18 +38,15 @@ class AddPass(BaseModel):
     images: List[Image]
 
     def add_pass(self):
-        connection = psycopg2.connect(user=FSTR_DB_LOGIN,
-                                      password=FSTR_DB_PASS,
-                                      host=FSTR_DB_HOST,
-                                      port=FSTR_DB_PORT,
-                                      database=FSTR_DB_NAME)
+        connection = connect_db()
+
         with connection:
             with connection.cursor() as cursor:
                 # Проверка, если ли юзер в базе данных
                 cursor.execute("SELECT id FROM users WHERE email=%s", (self.user.email,))
                 user_id = cursor.fetchone()
 
-                if user_id == None:
+                if not user_id:
                     # Вставить данные в таблицу Users
                     insert_user_data = "INSERT INTO users (name, email, phone) VALUES (%s, %s, %s) RETURNING id;"
                     cursor.execute(insert_user_data, (self.user.name, self.user.email, self.user.phone))
@@ -78,14 +66,12 @@ class AddPass(BaseModel):
                 # Получить id перевала
                 pass_id = cursor.fetchone()
 
-                #Вставить данные в таблицу Images
+                # Вставить данные в таблицу Images
                 for image in self.images:
                     insert_image_data = "INSERT INTO images (title, url_path, data_added, pass_id) VALUES (%s, %s, %s, %s);"
                     cursor.execute(insert_image_data, (image.title, image.url_path, image.data_added, pass_id))
 
         connection.close()
-        print("Данные успешно добавлены. Соединение с базой данных закрыто")
-
         return pass_id[0]
 
 
@@ -96,11 +82,8 @@ class Response(BaseModel):
 
 
 def pass_details(pass_id):
-    connection = psycopg2.connect(user=FSTR_DB_LOGIN,
-                                  password=FSTR_DB_PASS,
-                                  host=FSTR_DB_HOST,
-                                  port=FSTR_DB_PORT,
-                                  database=FSTR_DB_NAME)
+    connection = connect_db()
+
     with connection:
         with connection.cursor() as cursor:
             cursor.execute('''SELECT beauty_title, title, other_titles, title_connect, data_added, status, level_winter,
@@ -108,51 +91,47 @@ def pass_details(pass_id):
                            (pass_id,))
             columns = [column[0] for column in cursor.description]
             pass_data = cursor.fetchone()
-            pass_data = dict(zip(columns, pass_data))
-
+            if pass_data:
+                pass_data = dict(zip(columns, pass_data))
     connection.close()
 
     return pass_data
 
 
-
 def get_pass(pass_id):
-    connection = psycopg2.connect(user=FSTR_DB_LOGIN,
-                                  password=FSTR_DB_PASS,
-                                  host=FSTR_DB_HOST,
-                                  port=FSTR_DB_PORT,
-                                  database=FSTR_DB_NAME)
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute('''SELECT users.name, users.email, users.phone FROM users, passes WHERE 
-            passes.user_id=users.id AND passes.id=%s''', (pass_id,))
-            columns = [column[0] for column in cursor.description]
-            user_data = cursor.fetchone()
-            user_data = dict(zip(columns, user_data))
-            cursor.execute('''SELECT images.title, images.url_path, images.data_added FROM images, passes WHERE 
-            images.pass_id=passes.id AND passes.id=%s''', (pass_id,))
-            columns = [column[0] for column in cursor.description]
-            rows = cursor.fetchall()
-            image_data = []
-            for row in rows:
-                image_dict = dict(zip(columns, row))
-                image_data.append(image_dict)
-
-    connection.close()
-    print("Данные успешно извлечены. Соединение с базой данных закрыто")
-
-    return {"user": user_data, "passes": pass_details(pass_id), "images": image_data}
+    try:
+        connection = connect_db()
+    except:
+        raise HTTPException(status_code=500, detail="Ошибка подключения к базе данных")
+    else:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute('''SELECT users.name, users.email, users.phone FROM users, passes WHERE 
+                passes.user_id=users.id AND passes.id=%s''', (pass_id,))
+                columns = [column[0] for column in cursor.description]
+                user_data = cursor.fetchone()
+                if not user_data:
+                    raise HTTPException(status_code=404, detail="Перевал не найден")
+                else:
+                    user_data = dict(zip(columns, user_data))
+                cursor.execute('''SELECT images.title, images.url_path, images.data_added FROM images, passes WHERE 
+                images.pass_id=passes.id AND passes.id=%s''', (pass_id,))
+                columns = [column[0] for column in cursor.description]
+                rows = cursor.fetchall()
+                image_data = []
+                for row in rows:
+                    image_dict = dict(zip(columns, row))
+                    image_data.append(image_dict)
+        connection.close()
+        return {"user": user_data, "passes": pass_details(pass_id), "images": image_data}
 
 
 class PassOptional(Pass):
     __annotations__ = {k: Optional[v] for k, v in Pass.__annotations__.items()}
 
     def update_pass(self, pass_id):
-        connection = psycopg2.connect(user=FSTR_DB_LOGIN,
-                                      password=FSTR_DB_PASS,
-                                      host=FSTR_DB_HOST,
-                                      port=FSTR_DB_PORT,
-                                      database=FSTR_DB_NAME)
+        connection = connect_db()
+
         with connection:
             with connection.cursor() as cursor:
                 cursor.execute('''UPDATE passes SET beauty_title=%s, title=%s, other_titles=%s, title_connect=%s,
@@ -161,9 +140,7 @@ class PassOptional(Pass):
                                 self.title, self.other_titles, self.title_connect, self.data_added,
                                 self.level_winter, self.level_spring, self.level_summer, self.level_autumn,
                                 self.latitude, self.longitude, self.height, pass_id,))
-
         connection.close()
-        print("Данные успешно переданы. Соединение с базой данных закрыто")
 
 
 class ResponseUpdate(BaseModel):
@@ -172,28 +149,28 @@ class ResponseUpdate(BaseModel):
 
 
 def get_user_passes(user_email):
-    connection = psycopg2.connect(user=FSTR_DB_LOGIN,
-                                  password=FSTR_DB_PASS,
-                                  host=FSTR_DB_HOST,
-                                  port=FSTR_DB_PORT,
-                                  database=FSTR_DB_NAME)
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute('''SELECT passes.beauty_title, passes.title, passes.other_titles, passes.title_connect, 
-                passes.data_added, passes.level_winter, passes.level_spring, passes.level_summer, passes.level_autumn, 
-                passes.latitude, passes.longitude, passes.height FROM passes, users WHERE 
-                passes.user_id=users.id AND users.email=%s''', (user_email,))
-            columns = [column[0] for column in cursor.description]
-            rows = cursor.fetchall()
-            passes_data = []
-            for row in rows:
-                passes_dict = dict(zip(columns, row))
-                passes_data.append(passes_dict)
-
-    connection.close()
-    print("Данные успешно извлечены. Соединение с базой данных закрыто")
-
-    return passes_data
+    try:
+        connection = connect_db()
+    except:
+        raise HTTPException(status_code=500, detail="Ошибка подключения к базе данных")
+    else:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute('''SELECT passes.beauty_title, passes.title, passes.other_titles, passes.title_connect, 
+                    passes.data_added, passes.level_winter, passes.level_spring, passes.level_summer, passes.level_autumn, 
+                    passes.latitude, passes.longitude, passes.height FROM passes, users WHERE 
+                    passes.user_id=users.id AND users.email=%s''', (user_email,))
+                columns = [column[0] for column in cursor.description]
+                rows = cursor.fetchall()
+                if not rows:
+                    raise HTTPException(status_code=404, detail=f"Пользователь с email '{user_email}' не найден")
+                else:
+                    passes_data = []
+                    for row in rows:
+                        passes_dict = dict(zip(columns, row))
+                        passes_data.append(passes_dict)
+        connection.close()
+        return passes_data
 
 
 
